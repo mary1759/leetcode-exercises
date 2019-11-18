86e147803161e0c15fcb8ed4538be4f84daaa11b
Sai Kiran Reddy Maryada <44653871+mary1759@users.noreply.github.com>
2019-11-17 20:55:06 -0600

    Create twosum.py

---
+++ twosum.py
@@ -0,0 +1,13 @@
+class Solution:         #Brute Force Method.
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            j = i+1
+            for j in range(len(nums)):
+                if(nums[j] == target-nums[i]):
+                    l = [i, j]
+                    return l
+            
+            
+s = Solution()      
+p = s.twoSum([2,  7, 9, 11, 15], 9)
+print(p)
